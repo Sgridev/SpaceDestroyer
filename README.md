747395af229bac32fdbe20b3ae9e993241eaaf34
Sgri <35015674+Sgridev@users.noreply.github.com>
2019-03-20 10:12:42 +0100

    v2.0

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SpaceDestroyer
-A small vb.net solution that remove unnecessary spaces in your code
+A small c# library that removes unnecessary spaces in your file
  ![alt text](https://cdnb.artstation.com/p/assets/images/images/009/989/257/large/masch-artartist-space-destroyer-arbarty-tec-draw.jpg?1521974101)
---
+++ SpaceDestroyer2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceDestroyer2
+{
+    class SpaceDestroyer2
+    {
+
+        public void Clean()
+        {
+            do
+            {
+                string fileName = SelectFile();
+                bool result = false;
+                if (fileName != "")
+                    result = CleanFile(fileName);
+                if (result)
+                    Console.WriteLine("SUCCESS");
+
+            } while (AskAnother());
+        }
+   
+        private static string SelectFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            Console.WriteLine("CHOOSE FILE TO CLEAN");
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+                return fileDialog.FileName;
+            else
+                return "";
+        }
+
+     
+        private static bool CleanFile(string fileName)
+        {
+            Console.WriteLine("CLEANING......");
+            try
+            {
+                List<string> outputLines = new List<string>();
+                List<string> lines = System.IO.File.ReadAllLines(fileName).ToList();
+                foreach (string line in lines)
+                {
+                    string tempLine = line;
+                    while (tempLine.IndexOf("  ") != -1)
+                        tempLine = tempLine.Replace("  ", " ");
+                    outputLines.Add(tempLine);
+                }
+                System.IO.File.Delete(fileName);
+                System.IO.File.WriteAllLines(fileName, outputLines);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool AskAnother()
+        {
+            Console.WriteLine("CLEAN ANOTHER FILE?");
+            var result = "";
+            do
+                result = Console.ReadLine().ToUpper().TrimEnd();
+            while (result != "Y" && result != "N");
+            if (result == "Y")
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
